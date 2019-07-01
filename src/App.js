d7ecc5d7f77159e049cd9619baa19e5c48c7e1ff
Minwoo-Kang <sheepbomb@naver.com>
2019-07-01 21:55:02 +0900

    Create TodoItem component & Add sample TodoItem in TodoItemList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import TodoListTemplate from "./component/TodoListTemplate";
 import Form from "./component/Form"
+import TodoItemList from "./component/TodoItemList";
 
 
 class App extends Component {
     render() {
         return (
             <TodoListTemplate form={<Form />}>
-                Template completed !
+                <TodoItemList />
             </TodoListTemplate>
     );
     }
